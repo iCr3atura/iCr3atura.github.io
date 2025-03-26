b391a3e77db2b4f97deabe2179fa4c5cb115f6c5
iCr3atura <funnyrockable05@gmail.com>
2025-03-26 07:26:33 +0300

    new patch

--- js/app.js
+++ js/app.js
@@ -256,8 +256,8 @@ async function loadSheet(device) {
   const progression = main_prog.split(/\s+/).length;
 
   const variables = {
-    age, sex, totalLenght: totalLength, totalWords: wordCount, symbol, count, // total_gr, total_rak, total_traum,
-    // total_smert, total_hron, total_lom, addiction, stds: stds_count, vetryanka, allergies, progression
+    age, sex, totalLenght: totalLength, totalWords: wordCount, symbol, count, total_gr, total_rak, total_traum,
+    total_smert, total_hron, total_lom, addiction, stds: stds_count, vetryanka, allergies, progression
   };
 
   console.log(variables)
